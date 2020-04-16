19a4230df7123356d29b71354509eba76a7634d9
Allan <allan.g.galdino@outlook.com>
2020-04-16 11:30:23 -0300

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Allan on 16/04/20.
+//  Copyright © 2020 Allan Galdino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
